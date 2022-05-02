9547e75ef03fac1a6c2434ca90e7671dc2658d7e
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2022-05-02 12:49:12 +0200

    přidání výjimek

--- get_data.py
+++ get_data.py
@@ -1,5 +1,20 @@
 # skript na automaticke stazeni a rozbaleni dat GTFS
 import requests, zipfile, io
-r = requests.get("http://data.pid.cz/PID_GTFS.zip")
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall("gtfs")
+
+def get_data(file_name):
+    try:
+        r = requests.get("http://data.pid.cz/PID_GTFS.zip")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall("gtfs")
+    except FileNotFoundError:
+        print(f"Cannot open file {file_name}. The file does not exist or the path to the file is incorrect")
+        quit()
+    except PermissionError:
+        print(f"Program doesn't have permisson to access file {file_name}.")
+        quit() 
+    except zipfile.BadZipFile:
+        print(f"{file_name} is not a ZIP file.")
+        quit()
+    except zipfile.LargeZipFile:
+        print(f"{file_name} would require ZIP64 functionality to be unzipped but that has not been enabled (yet).")
+
