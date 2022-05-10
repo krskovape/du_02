34626dbfe95633bb2cee7bdeb9dcf5d1960357bb
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2022-05-10 11:53:07 +0200

    doplnění výjimek

--- get_data.py
+++ get_data.py
@@ -1,23 +1,35 @@
 # skript na automaticke stazeni a rozbaleni dat GTFS
+from urllib.error import HTTPError
 import requests, zipfile, io
+
+from urllib3 import Timeout
 
 def retrieve_data():
     try:
-        print("Downloading data.")
+        print("Stahuju data.")
         r = requests.get("http://data.pid.cz/PID_GTFS.zip")
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall("gtfs")
-        print("Done.")
+        print("Hotovo.")
 
     except FileNotFoundError:
-        print("Cannot open file. The file does not exist or the path to the file is incorrect")
+        print("Nelze otevřít soubor. Vybraný soubor buď neexistuje nebo je k němu zadána nekorektní cesta.")
         quit()
     except PermissionError:
-        print("Program doesn't have permisson to access file.")
+        print("Program nemá povolení k přístupu k souboru.")
         quit() 
     except zipfile.BadZipFile:
-        print("File is not a ZIP file.")
+        print("Soubor není .zip soubor.")
         quit()
     except zipfile.LargeZipFile:
-        print("File is too big for this program to unzipp it.")
+        print("Soubor je pro program příliš velký na to, aby ho odzipoval.")
+        quit()
+    except ConnectionError:
+        print("Chybí připojení k internetu.")
+        quit()
+    except HTTPError:
+        print("Neúspěšný pokus o stažení dat.")
+        quit()
+    except TimeoutError:
+        print("Vypršel čas požadavku.")
         quit()
