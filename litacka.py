b4abc8c303cf97ee158e21c352034ac9e364bb2b
krskovape <krskovape@natur.cuni.cz>
2022-05-15 16:17:52 +0200

    Issue #12

--- litacka.py
+++ litacka.py
@@ -51,13 +51,13 @@ class Stop(GTFSTable):
     def __init__(self, data):
         self.stop_id : str = data["stop_id"]
         self.stop_name : str = data["stop_name"]
-        self.stop_lat : float = data["stop_lat"]
-        self.stop_lon : float = data["stop_lon"]
+        self.stop_lat : float = float(data["stop_lat"])
+        self.stop_lon : float = float(data["stop_lon"])
         self.zone_id : str = data["zone_id"]
         self.stop_url : str = data["stop_url"]   
-        self.location_type : int = data["location_type"]  
+        self.location_type : int = int(data["location_type"])  
         self.parent_station : str = data["parent_station"]
-        self.wheelchair_boarding : int = data["wheelchair_boarding"]
+        self.wheelchair_boarding : int = int(data["wheelchair_boarding"])
         self.level_id : str = data["level_id"]
         self.platform_code : str = data["platform_code"]
         self.asw_node_id : int = data["asw_node_id"]
@@ -82,13 +82,13 @@ class StopTime(GTFSTable):
         self.arrival_time : time = data["arrival_time"]
         self.departure_time : time = data["departure_time"]
         self.stop : Stop = dict_stops.get(data["stop_id"])
-        self.stop_sequence : int = data["stop_sequence"]
+        self.stop_sequence : int = int(data["stop_sequence"])
         self.stop_headsign : str = data["stop_headsign"]
-        self.pickup_type : int = data["pickup_type"]
-        self.drop_off_type : int = data["drop_off_type"]
-        self.shape_dist_traveled : float = data["shape_dist_traveled"]
-        self.trip_operation_type : int = data["trip_operation_type"]
-        self.bikes_allowed : int = data["bikes_allowed"]
+        self.pickup_type : int = int(data["pickup_type"])
+        self.drop_off_type : int = int(data["drop_off_type"])
+        self.shape_dist_traveled : float = float(data["shape_dist_traveled"])
+        self.trip_operation_type : int = int(data["trip_operation_type"])
+        self.bikes_allowed : int = int(data["bikes_allowed"])
 
     #method for creating elements, returns list of elements and dictionary for connecting classes    
     @classmethod
@@ -108,12 +108,12 @@ class Trip(GTFSTable):
         self.trip_id : str = data["trip_id"]
         self.trip_headsign : str = data["trip_headsign"]
         self.trip_short_name : str = data["trip_short_name"]
-        self.direction_id : int = data["direction_id"]
+        self.direction_id : int = int(data["direction_id"])
         self.block_id : str = data["block_id"]
         self.shape_id : str = data["shape_id"]
-        self.wheelchair_accessible : int = data["wheelchair_accessible"]
-        self.bikes_allowed : int = data["bikes_allowed"]
-        self.exceptional : int = data["exceptional"]
+        self.wheelchair_accessible : int = int(data["wheelchair_accessible"])
+        self.bikes_allowed : int = int(data["bikes_allowed"])
+        self.exceptional : int = int(data["exceptional"])
 
     #method for creating elements, returns list of elements and dictionary for connecting classes    
     @classmethod
@@ -134,13 +134,13 @@ class Route(GTFSTable):
         self.agency_id = data["agency_id"]
         self.route_short_name : str = data["route_short_name"]
         self.route_long_name : str = data["route_long_name"]
-        self.route_type : int = data["route_type"]
+        self.route_type : int = int(data["route_type"])
         self.route_url : str = data["route_url"]
         self.route_color : str = data["route_color"]
         self.route_text_color : str = data["route_text_color"]
-        self.is_night : int = data["is_night"]
-        self.is_regional : int = data["is_regional"]
-        self.is_substitute_transport : int = data["is_substitute_transport"]
+        self.is_night : int = int(data["is_night"])
+        self.is_regional : int = int(data["is_regional"])
+        self.is_substitute_transport : int = int(data["is_substitute_transport"])
 
     #method for creating elements, returns list of elements and dictionary for connecting classes    
     @classmethod
